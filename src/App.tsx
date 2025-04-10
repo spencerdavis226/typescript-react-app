d2867717a978b59f0f6de231c7801e82ca567837
Spencer Davis <spencerdavis226@gmail.com>
2025-04-10 11:46:29 -0400

    Created generic component

--- src/App.tsx
+++ src/App.tsx
@@ -32,18 +32,38 @@ function App() {
     }
   }, [addTodo]);
 
+  function UL<T>({
+    items,
+    render,
+  }: {
+    items: T[];
+    render: (item: T) => React.ReactNode;
+  }) {
+    return (
+      <ul>
+        {items.map((item, index) => (
+          <li key={index}>{render(item)}</li>
+        ))}
+      </ul>
+    );
+  }
+
   return (
     <div>
       <Heading title="Introduction" />
       <Box>Hello there</Box>
 
       <Heading title="Todos" />
-      {todos.map((todo) => (
-        <div key={todo.id}>
-          {todo.text}
-          <button onClick={() => removeTodo(todo.id)}>Remove</button>
-        </div>
-      ))}
+      <UL
+        items={todos}
+        render={(todo) => (
+          <>
+            {todo.text}
+            <button onClick={() => removeTodo(todo.id)}>Remove</button>
+          </>
+        )}
+      />
+
       <div>
         <input type="text" ref={newTodoRef} />
         <button onClick={onAddTodo}>Add Todo</button>
