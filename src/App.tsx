bdfecd2b35889052b2bba06ef9ca6adad2fe93dc
Spencer Davis <spencerdavis226@gmail.com>
2025-04-10 10:54:40 -0400

    Abstracted Todos into useTodos.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,6 @@
-import React, {
-  Children,
-  useCallback,
-  useEffect,
-  useReducer,
-  useRef,
-  useState,
-} from 'react';
+import React, { useCallback, useRef } from 'react';
 import './App.css';
+import { useTodos } from './useTodos';
 
 const Heading = ({ title }: { title: string }) => {
   return <h2>{title}</h2>;
@@ -25,117 +19,29 @@ const Box = ({ children }: { children: React.ReactNode }) => {
   );
 };
 
-const List = ({
-  items,
-  onClick,
-}: {
-  items: string[];
-  onClick?: (item: string) => void;
-}) => {
-  return (
-    <ul>
-      {items.map((item, index) => (
-        <li key={index} onClick={() => onClick?.(item)}>
-          {item}
-        </li>
-      ))}
-    </ul>
-  );
-};
-
-interface Payload {
-  text: string;
-}
-
-interface Todo {
-  id: number;
-  done: boolean;
-  text: string;
-}
-
-type ActionType =
-  | { type: 'ADD'; text: string }
-  | { type: 'REMOVE'; id: number };
-
-const useNumber = (initialValue: number) => useState<number>(initialValue);
-
-type UseNumberValue = ReturnType<typeof useNumber>[0];
-type UseNumberSetValue = ReturnType<typeof useNumber>[1];
-
-const Incrementer: React.FC<{
-  value: UseNumberValue;
-  setValue: UseNumberSetValue;
-}> = ({ value, setValue }) => {
-  return <button onClick={() => setValue(value + 1)}>Add - {value}</button>;
-};
-
-const Button: React.FC<> = ({ children }) => <button>{children}</button>;
-
 function App() {
-  const onListClick = useCallback((item: string) => {
-    alert(item);
-  }, []);
-
-  const [payload, setPayload] = useState<Payload | null>(null);
-
-  useEffect(() => {
-    fetch('../public/payload.json')
-      .then((resp) => resp.json())
-      .then((data) => {
-        setPayload(data);
-      });
-  }, []);
-
-  const [todos, dispatch] = useReducer(
-    (state: Todo[], action: ActionType): Todo[] => {
-      switch (action.type) {
-        case 'ADD':
-          return [
-            ...state,
-            {
-              id: state.length,
-              text: action.text,
-              done: false,
-            },
-          ];
-        case 'REMOVE':
-          return state.filter(({ id }) => id !== action.id);
-        default:
-          throw new Error();
-      }
-    },
-    []
-  );
-
+  const { todos, addTodo, removeTodo } = useTodos([
+    { id: 0, text: 'Hey there!', done: false },
+  ]);
   const newTodoRef = useRef<HTMLInputElement>(null);
 
   const onAddTodo = useCallback(() => {
     if (newTodoRef.current) {
-      dispatch({
-        type: 'ADD',
-        text: newTodoRef.current.value,
-      });
+      addTodo(newTodoRef.current.value);
       newTodoRef.current.value = '';
     }
-  }, []);
-
-  const [value, setValue] = useNumber(0);
+  }, [addTodo]);
 
   return (
     <div>
       <Heading title="Introduction" />
       <Box>Hello there</Box>
-      <List items={['one', 'two', 'three']} onClick={onListClick} />
-      <Box>{JSON.stringify(payload)}</Box>
-      <Incrementer value={value} setValue={setValue} />
 
       <Heading title="Todos" />
       {todos.map((todo) => (
         <div key={todo.id}>
           {todo.text}
-          <button onClick={() => dispatch({ type: 'REMOVE', id: todo.id })}>
-            Remove
-          </button>
+          <button onClick={() => removeTodo(todo.id)}>Remove</button>
         </div>
       ))}
       <div>
